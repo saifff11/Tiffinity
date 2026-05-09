d87f48c15a3405c9c3d8e76d561d221f4df9b421
saifff11 <mdsaifali0291@gmail.com>
2026-05-09 23:48:03 +0530

    Fixing OTP Verification

--- server/utils/createAdmin.js
+++ server/utils/createAdmin.js
@@ -6,14 +6,19 @@ dotenv.config()
 
 const createAdmin = async () => {
   try {
+
     await mongoose.connect(process.env.MONGO_URI)
 
+    console.log('MongoDB Connected')
+
     const adminExists = await User.findOne({ role: 'admin' })
+
     if (adminExists) {
+      console.log('Admin already exists')
       process.exit()
     }
 
-    await User.create({
+    const admin = await User.create({
       name: process.env.ADMIN_NAME,
       email: process.env.ADMIN_EMAIL,
       password: process.env.ADMIN_PASSWORD,
@@ -22,9 +27,16 @@ const createAdmin = async () => {
       isVerified: true
     })
 
+    console.log('Admin Created Successfully')
+    console.log(admin)
+
     process.exit()
 
   } catch (error) {
+
+    console.log('ERROR:')
+    console.log(error)
+
     process.exit(1)
   }
 }
